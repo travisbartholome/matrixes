e6bea7370f53e24daf8bc6c42bce49e97284e40c
travisbartholome <travis.bartholome@gmail.com>
2017-01-13 18:10:54 -0600

    Started refactoring matrix.js, will continue later

--- matrix.js
+++ matrix.js
@@ -1,28 +1,62 @@
-const matrix = module.exports;
+const Matrix = module.exports;
 
-// Creator functions
-matrix.createMatrix = require('./functions/createMatrix.js');
-matrix.identity = require('./functions/identity.js');
-matrix.zeros = require('./functions/zeros.js');
+Matrix.add = require('./functions/add.js');
+Matrix.copy = require('./functions/copy.js');
+Matrix.createMatrix = require('./functions/createMatrix.js');
+Matrix.det = require('./functions/det.js');
+Matrix.equals = require('./functions/equals.js');
+Matrix.getPrecision = require('./functions/precision.js').getPrecision;
+Matrix.identity = require('./functions/identity.js');
+Matrix.inverse = require('./functions/inverse.js');
+Matrix.is2DArray = require('./functions/is2DArray.js');
+Matrix.isRectangular = require('./functions/isRectangular.js');
+Matrix.isSquare = require('./functions/isSquare.js');
+Matrix.isValidMatrix = require('./functions/isValidMatrix.js');
+Matrix.multiply = require('./functions/multiply.js');
+Matrix.reduce = require('./functions/reduce.js');
+Matrix.reduceAug = require('./functions/reduceAug.js');
+Matrix.scale = require('./functions/scale.js');
+Matrix.setPrecision = require('./functions/precision.js').setPrecision;
 
-// Operations
-matrix.add = require('./functions/add.js');
-matrix.det = require('./functions/det.js');
-matrix.inverse = require('./functions/inverse.js');
-matrix.multiply = require('./functions/multiply.js');
-matrix.reduce = require('./functions/reduce.js');
-matrix.reduceAug = require('./functions/reduceAug.js');
-matrix.scale = require('./functions/scale.js');
-matrix.subtract = require('./functions/subtract.js');
+const isValidMatrix = require('./functions/isValidMatrix.js'); // TODO: Remove this
+Matrix.subtract = function subtract(matrixOne, matrixTwo) {
+  if (!isValidMatrix(matrixOne) || !isValidMatrix(matrixTwo)) {
+    throw new Error('Invalid matrix');
+  }
 
-// Assertions
-matrix.equals = require('./functions/equals.js');
-matrix.is2DArray = require('./functions/is2DArray.js');
-matrix.isRectangular = require('./functions/isRectangular.js');
-matrix.isSquare = require('./functions/isSquare.js');
-matrix.isValidMatrix = require('./functions/isValidMatrix.js');
+  if (matrixOne.length !== matrixTwo.length || matrixOne[0].length !== matrixTwo[0].length) {
+    throw new Error('Matrices must have the same dimensions');
+  }
 
-// Other methods
-matrix.copy = require('./functions/copy.js');
-matrix.getPrecision = require('./functions/precision.js').getPrecision;
-matrix.setPrecision = require('./functions/precision.js').setPrecision;
+  let output = [];
+  for (let i = 0; i < matrixOne.length; i++) {
+    output.push([]);
+    for (let j = 0; j < matrixOne[0].length; j++) {
+      output[i].push(matrixOne[i][j] - matrixTwo[i][j]);
+    }
+  }
+  return output;
+}
+
+// --
+
+Matrix.zeros = function zeros(numRows, numColumns) {
+  if (!Number.isInteger(numRows) || !Number.isInteger(numColumns)) {
+    // Consider using Number.isSafeInteger here.
+    throw new Error('Invalid matrix size: both row size and column size must be an integer');
+  }
+
+  if (numRows <= 0 || numColumns <= 0) {
+    throw new Error('Invalid matrix size: both row size and column size must be greater than 0');
+  }
+
+  let matrix = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numColumns; j++) {
+      row.push(0)
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
