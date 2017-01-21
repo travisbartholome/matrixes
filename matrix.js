da396ecbe1cb4be31811f038e40bd36f941f6fee
travisbartholome <travis.bartholome@gmail.com>
2017-01-21 15:17:24 -0600

    More forgiving of extra whitespace in #createMatrix

--- matrix.js
+++ matrix.js
@@ -143,7 +143,7 @@ Matrix.augment = augment;
 function createMatrix(input) {
   if (typeof input !== 'string') throw new Error('Input to createMatrix must be a string');
 
-  let matrix = input.replace(/, /g, ',').split(',').map(function(string) {
+  let matrix = input.replace(/[ ]*,[ ]+/g, ',').split(',').map(function(string) {
     return string.split(' ').map(function(entry) {
       let number = parseFloat(entry);
       if (isNaN(number) || !isFinite(number)) {
